b9cf7cfa4756e1f74d0ff8411c6459796526bc13
Mayank <mayankkashyap95@gmail.com>
2016-01-04 19:23:50 +0530

    edits by Mayank

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,17 +1,17 @@
 class RegisterController < ApplicationController
-  def new
-  end
-  def create
-  @registered = Registered.create(registered_params)
-  @events = params["event"]["id"]
-  @events.each do |e|
-    @er = EventRegistered.create(event_id: e, registered_id: @registered.id)
-  end
-redirect_to '/'
-end
-private
-def registered_params
-  params.require(:register).permit(:name,:college,:branch,:year,:gender,:email,:mobile)
-end
+	def new
+	end
+	def create
+		@registered = Registered.create(registered_params)
+		@events = params["event"]["id"]
+		@events.each do |e|
+			@er = EventRegistered.create(event_id: e, registered_id: @registered.id)
+		end
+		redirect_to '/'
+	end
+	private
+	def registered_params
+		params.require(:register).permit(:name,:college,:branch,:year,:gender,:email,:mobile)
+	end
 
 end
